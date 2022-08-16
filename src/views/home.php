ac5963f69a820dbe7d3840eb048a776424f6c172
ShiroAky <martinezfranklinjunior7@gmail.com>
2022-08-15 20:05:49 -0400

    template

--- src/views/home.php
+++ src/views/home.php
@@ -1,10 +1,8 @@
 <?php require_once './src/app/Render.php'; ?>
 <?php require_once './src/controllers/URL.php'; ?>
-<?php require_once './src/models/Animes.php'; ?>
 
 <?php use App\Render; ?>
 <?php use Controllers\URL; ?>
-<?php use Models\Animes; ?>
 
 <?php  ?>
 
@@ -14,12 +12,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AnimeLine</title>
+    <title>Home</title>
     <!-- Styles css -->
-    <link rel="stylesheet" href="<?php URL::for('css', 'normalize.css'); ?>">
-    <link rel="stylesheet" href="<?php URL::for('css', 'general.css'); ?>">
+    <link rel="stylesheet" href="<?php URL::url_for('css', 'normalize.css'); ?>">
+    <link rel="stylesheet" href="<?php URL::url_for('css', 'general.css'); ?>">
     <!-- Favicon -->
-    <link rel="shortcut icon" href="<?php URL::for('img', 'logo.png'); ?>" type="image/png">
+    <link rel="shortcut icon" href="<?php URL::url_for('img', 'logo.png'); ?>" type="image/png">
 </head>
 <body>
 
@@ -29,23 +27,8 @@
     <!-- Navbar -->
     <?php Render::module('nav'); ?>
 
-    <?php $result = Animes::getAnimes(20); ?>
-
-    <!-- Main -->
-    <main class="auto-grid">
-
-        <?php // foreach ($result as $anime) : ?>
-
-            <?php // Render::module('anime-item', $anime); ?>
-
-        <?php // endforeach; ?>
-
-    </main>
-
-    <?php ?>
-
     <!-- Scripts -->
-    <script src="<?php URL::for('js', 'main.js'); ?>"></script>
+    <script src="<?php URL::url_for('js', 'main.js'); ?>"></script>
     
 </body>
 </html>
