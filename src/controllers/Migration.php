ecfe3fcaf4c262da1be5bab2d2de4e78c8b22930
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2022-10-12 00:34:52 -0400

    Update Migration.php

--- src/controllers/Migration.php
+++ src/controllers/Migration.php
@@ -1,1 +1,17 @@
+<?php
 
+    namespace App\Database\Migrations;
+
+    /**
+    * @package App\Database\Migrations\Content
+    */
+
+    class Content
+    {
+
+        public static function up()
+        {
+            
+        }
+
+    }
