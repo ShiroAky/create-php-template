c2eb5e4e2b48ea03a18f81d217ad8959b1edd92b
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2022-10-11 23:30:15 -0400

    Create Controller.php

---
+++ src/controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Controllers;
+
+    /**
+    * @package App\Controllers\Controller
+    */
+
+    class Controller
+    {
+
+        public static function index()
+        {
+            echo 'Success';
+        }
+
+    }
