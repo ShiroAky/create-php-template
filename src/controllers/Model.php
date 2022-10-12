5ae432c2d0a054836ba6b703bd24a7f32423fb9f
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2022-10-12 00:31:19 -0400

    Create Model.php

---
+++ src/controllers/Model.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Database\Models;
+
+    /**
+    * @package App\Database\Models\Content
+    */
+
+    class Content
+    {
+
+        public static function up()
+        {
+            
+        }
+
+    }
